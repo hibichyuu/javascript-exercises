647c267959c557f36fec0a5a2bd8cba095093af5
hibichyuu <hibichyuu@gmail.com>
2024-02-03 19:58:08 -0600

    Make removeFromArray.js pass tests given

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedElement) {
+    let newArray = [];
+    let concatable = true;
 
+    for(let i = 0; i < array.length; i++){
+
+        for(let element of removedElement){
+            if(array[i] === element) concatable = false;
+            if(!concatable) break;
+        }
+
+        if(concatable){
+            newArray = newArray.concat(array[i]);
+         }
+
+         concatable = true;
+     }
+    
+
+    return newArray;
 };
 
 // Do not edit below this line
