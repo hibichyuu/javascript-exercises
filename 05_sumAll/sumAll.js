d18e77fd3f5a66ce30729f8540231680a7376fac
hibichyuu <hibichyuu@gmail.com>
2024-02-03 20:47:08 -0600

    make sumAll.js pass tests given

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    let sum = -1;
+
+    if(Number.isInteger(min) && Number.isInteger(max) && min >= 0 && max >= 0){
+
+        if(min > max){
+            const temp = min;
+            min = max;
+            max = temp;
+        }
+
+        for(let i = min ; i <= max; i++){
+            sum += i;
+        }
+    }
+
+    return (sum == -1) ? 'ERROR': sum + 1;
 };
 
 // Do not edit below this line
