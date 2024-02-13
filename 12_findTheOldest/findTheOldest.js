50130c4b9307611990632a430c6eb88b73135808
hibichyuu <hibichyuu@gmail.com>
2024-02-13 13:37:16 -0600

    Make findTheOldest.js pass given tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people = []) {
+    people.sort((currGuy, nextGuy)=>{
 
+        const currentYear = new Date().getFullYear();
+
+        let currGuyAge = 
+            (!currGuy.yearOfDeath) ?
+            currentYear - currGuy.yearOfBirth :
+            currGuy.yearOfDeath - currGuy.yearOfBirth;
+        let nextGuyAge = 
+            (!nextGuy.yearOfDeath) ?
+            currentYear - nextGuy.yearOfBirth :
+            nextGuy.yearOfDeath - nextGuy.yearOfBirth;
+
+        return (currGuyAge > nextGuyAge) ? 1 : -1;
+    });
+
+return people[people.length - 1];
 };
 
 // Do not edit below this line
