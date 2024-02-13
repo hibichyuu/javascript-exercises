17f8f946019c3821ba1e54ade4ddeede5de2ee0f
hibichyuu <hibichyuu@gmail.com>
2024-02-13 07:29:03 -0600

    make calculator.js pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr = [0]) {
+	const total = arr.reduce((sum, currentValue)=>{
+    return sum + currentValue;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr = [0]) {
+  const total = arr.reduce((sum, currentValue)=>{
+    return sum * currentValue;
+  }, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num = 0, pow = 0) {
+	return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(factOf) {
+	let sum = 1;
+  for(let i = 1; i <= factOf; i++){
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
