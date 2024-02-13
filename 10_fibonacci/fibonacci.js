70127e82d81f3c5b1aebd3dc448280d73d9e6723
hibichyuu <hibichyuu@gmail.com>
2024-02-13 08:42:42 -0600

    Make fibonacci.js pass given tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    Number(num);
+    if(num < 0) return "OOPS";
+
+    let nextNum = 1;
+    let fibNum = 0;
+    let placeHolder = 0;
+
+    for(let i = 0; i < num; i++){
+        placeHolder = nextNum;
+        nextNum += fibNum;
+        fibNum = placeHolder;
+    }
+    return fibNum;
 };
 
 // Do not edit below this line
