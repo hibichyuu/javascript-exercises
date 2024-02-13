074005881bdb5efc7fcafab418f0cd8c7257c47a
hibichyuu <hibichyuu@gmail.com>
2024-02-13 08:25:58 -0600

    Make palindromes.js pass tests given

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (stringArg = '') {
+   let newString = stringArg.toLowerCase().replace(/[^a-z0-9]/g, ""); // copied from solution, cant figure out what /[^a-z0-9]/g meant.
+   return newString.split("").reverse().join("") === newString;
 };
 
 // Do not edit below this line
